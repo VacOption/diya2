a9cee2c189c0ab3b3cbf6893ec6aa71037f394ad
VacOption <88313649+VacOption@users.noreply.github.com>
2024-11-23 21:56:17 +0200

    Add files via upload

---
+++ diya_v2/access/values.js
@@ -0,0 +1,16 @@
+// Активировать документы (true - включены, false - отключены)
+var isRightsEnabled = false; // Права
+var isCovidCertificateEnabled = false; // COVID-сертификат
+
+// Общее
+var birthdate = "28.12.2005"; // Дата рождения
+var fullname = "Ільницький Артем Борисович" // ФИО
+var name = "Артем" // Имя
+
+// Паспорт
+var passport_id = "274827490" // Номер паспорта
+
+
+// COVID-сертификат
+var covid_valid_until = "01.01.2022" // Сертификат "Дійсний до"
+var covid_certificate_id = "URN:UVCI:01:UA:0<br>E55669376876888<br>B9E2520C4F88930" // Номер сертификата (<br> - перенос строки)
